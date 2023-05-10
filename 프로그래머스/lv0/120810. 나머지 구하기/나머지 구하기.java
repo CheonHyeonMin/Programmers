83ec95a52468a266f87fb9a0b2d45a8ff5e80109
천현민 <130362772+CheonHyeonMin@users.noreply.github.com>
2023-05-10 09:21:30 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer= num1%num2;
+        return answer;
+        // if( num1%num2 == 1){
+        //     return 1;
+        // }
+        // else if(num1%num2==0){
+        //     return 0;
+        // }
+        // else{
+        //     return -1;
+        // }
+    }
+}
