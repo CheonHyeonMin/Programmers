9291455723cf87c11dcb4cbe63daca99a9bf82f2
천현민 <130362772+CheonHyeonMin@users.noreply.github.com>
2023-07-27 00:56:34 +0900

    [level 0] Title: 배열 두배 만들기, Time: 5.97 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = numbers.map{it*2}.toIntArray()
+        
+        
+        
+        return answer
+    }
+}
