191ae0bcfb99e8f01183d01a8309fe7ab395cfca
천현민 <130362772+CheonHyeonMin@users.noreply.github.com>
2023-07-26 23:50:37 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(money: Int): IntArray {
+        var answer: IntArray = intArrayOf(0,0)
+        var ice = 5500
+        
+        
+        
+        return intArrayOf(money/ice , money%ice)
+    }
+}
