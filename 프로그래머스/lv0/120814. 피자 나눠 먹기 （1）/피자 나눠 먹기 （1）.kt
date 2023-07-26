9399ce5690b567fb1937cc26630e0e5bb7a70ec8
천현민 <130362772+CheonHyeonMin@users.noreply.github.com>
2023-07-26 23:37:33 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
@@ -1,15 +1,6 @@
 class Solution {
     fun solution(n: Int): Int {
         var answer: Int = 0
-        var pizza = 7
-        if(n%pizza==0){
-            return n/pizza
-        }
-        else{
-            return n/pizza +1
-        }
-        
-        
-        
+        return answer
     }
 }
