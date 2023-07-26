e083e4a54e9336334be246b46963a6ca00655970
천현민 <130362772+CheonHyeonMin@users.noreply.github.com>
2023-07-27 00:13:23 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 36.08 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(str: String, n: Int): String {
+        var answer: String = ""
+        val a = str.split("")
+        val s = StringBuilder()
+        for (i in a) {
+            for (j in 0 until n) {
+                s.append(i)
+            }
+        }
+        return s.toString()
+        
+        
+        
+        return answer
+    }
+}
